553b04b376a3c1efa222436f58c9b36ab6423f3e
Ross Edfort <rossedfort@gmail.com>
2021-09-21 09:36:34 -0600

    try diff conditional

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Prompt for git path
 vared -p 'Please specify your home development directory, i.e. /Users/you/dev: ' -c GIT_HOME
 
-if [[ ! -z "$GIT_HOME" ]]; then
+if [[ ! -z GIT_HOME ]]; then
   echo "Plese enter your home development directory when prompted."
   exit 1
 else
