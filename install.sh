45768ed6f79d4c31f87cb842aad32ddaba8ed587
Ross Edfort <rossedfort@gmail.com>
2021-09-21 09:15:44 -0600

    typo

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Prompt for git path
 vared -p 'Please specify your home development directory, i.e. /Users/you/dev: ' -c GIT_HOME
 
-if [[ ! -z "$GIT_HOME" ]]l then
+if [[ ! -z "$GIT_HOME" ]]; then
   echo "Plese enter your home development directory when prompted."
   exit 1
 else
@@ -18,7 +18,7 @@ echo "Installing Homebrew"
 /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # If git doesn't exist, install it.
-if [[ ! type "$git" > /dev/null ]]; then
+if ! type "$git" >/dev/null; then
   echo "Git not installed -- installing git"
   brew install git
 fi
@@ -49,8 +49,8 @@ ln -s "$DOTFILES_PATH/.gitignore_global" "$HOME"
 ln -s "$DOTFILES_PATH/vscode_settings.json" "$VSCODE_SETTINGS_PATH"
 
 # Make sure new machine includes my config
-echo "source \$DOTFILES_PATH/shell/.zshenv" >> "$HOME/.zshenv"
-echo "source \$DOTFILES_PATH/shell/.zshrc" >> "$HOME/.zshrc"
+echo "source \$DOTFILES_PATH/shell/.zshenv" >>"$HOME/.zshenv"
+echo "source \$DOTFILES_PATH/shell/.zshrc" >>"$HOME/.zshrc"
 
 # Increase key repeat speed
 defaults write -g InitialKeyRepeat -int 15
@@ -85,7 +85,7 @@ ssh-keygen \
 
 echo "Generated new key: $HOME/.ssh/$ssh_key_filename"
 echo "Copying new public key to clipboard, plz add to GitHub: https://github.com/settings/keys"
-pbcopy < "~/.ssh/$ssh_key_filename.pub"
+pbcopy <"~/.ssh/$ssh_key_filename.pub"
 echo "Don't forget to add your new key to the ssh-agent https://docs.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent"
 
 # start a new shell
